f0195e3f13d9f09bcac9feea2a6c95969e1bdf77
Catherine Mei <meic1212@mit.edu>
2024-10-22 01:16:00 -0400

    aspirin working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import type { Component } from "solid-js";
 import { TraversalOutputComponentKeyboardParentFocus } from "./priority-traversal-parent-focus";
 
-import { stackedBarChartHypergraph } from "./input-data";
+import { aspirinHypergraph } from "./input-aspirin";
 
 const App: Component = () => {
   return (
     <TraversalOutputComponentKeyboardParentFocus
-      nodeGraph={stackedBarChartHypergraph}
+      nodeGraph={aspirinHypergraph}
       showHypergraph={false}
     />
   );
